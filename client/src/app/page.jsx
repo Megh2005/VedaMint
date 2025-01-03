154ac79f840239f57870c2d27fe1e7d03007190b
Subham Mani <manisubham09@gmail.com>
2025-01-04 00:20:15 +0530

    update landing page

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -21,7 +21,7 @@ export default function AuroraBackgroundDemo() {
 
   const connectWallet = async () => {
     if (!window.ethereum) {
-      toast.error('Wallet Not Found');
+      toast.error("Wallet Not Found");
       return;
     }
 
@@ -38,42 +38,45 @@ export default function AuroraBackgroundDemo() {
       const sepoliaNetworkId = "43113";
 
       if (chainId.toString() !== sepoliaNetworkId) {
-        toast.error('Please Switch To Avalanche Fuji Testnet');
-      }
-      else {
-        toast.success('Wallet Connected');
+        toast.error("Please Switch To Avalanche Fuji Testnet");
+      } else {
+        toast.success("Wallet Connected");
         setTimeout(() => {
           router.push("/about");
         }, 3000);
       }
     } catch (error) {
-      toast.error("Error In Connecting Wallet")
+      toast.error("Error In Connecting Wallet");
       console.error("Connection error:", error);
     }
   };
 
   return (
-    (<AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4">
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Own Your Credentials With Ownio
-        </div>
-        <button onClick={connectWallet} className="p-[3px] relative">
-          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-          <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-            {isConnected
-              ? `${userAddress.slice(0, 12)}...${userAddress.slice(-13)}` : 'Connect With Metamask'}
+    <AuroraBackground>
+      <div className="relative overflow-hidden pt-40 lg:pt-64 pb-60">
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <div className="flex justify-center">
+              <h1 className="w-max pb-2 animated-underline fade-pullup text-4xl sm:text-5xl lg:text-7xl font-bold mb-6">
+                Ownio
+              </h1>
+            </div>
+            <p className="text-xl text-slate-300 mb-8 max-w-2xl mx-auto">
+              Empowering creators through secure and effortless NFT publishing
+            </p>
+            <div className="flex justify-center gap-4">
+              <button
+                onClick={connectWallet}
+                className="font-bold max-w-sm flex items-center justify-center bg-purple-600 text-white px-8 py-3 rounded-lg hover:bg-purple-700 transition-colors"
+              >
+                {isConnected
+                  ? `${userAddress.slice(0, 12)}...${userAddress.slice(-13)}`
+                  : "Connect With Metamask"}
+              </button>
+            </div>
           </div>
-        </button>
-      </motion.div>
-    </AuroraBackground>)
+        </div>
+      </div>
+    </AuroraBackground>
   );
 }
